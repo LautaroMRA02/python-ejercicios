d86005d2c960a3678f452af6659ea55206b5bf8f
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2021-09-20 23:43:57 -0300

    ejercicio 004: sacar area de un circulo

---
+++ primera_parte/ex004_area_circulo.py
@@ -0,0 +1,11 @@
+#sacar area de un circulo 
+from math import pi 
+
+
+r = float(input('Escriba el radio del circulo: '))
+
+
+area = pi * r ** 2
+
+
+print('El area del circulo es {}'.format(area))
