8a616e667fb55a55d5f4812fa8e2d700eb24da86
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2021-09-20 23:18:57 -0300

    ex002 los tipos de prints()

---
+++ primera_parte/ex002_print.py
@@ -0,0 +1,26 @@
+print('este es un ejemplo Basico.')
+
+print('Este esun ejemple basico.', end=' ')
+print('Este esun ejemple basico.', end=' ')
+
+print()
+
+print('Python', 'es', 'tremendo!')
+print('Python', 'es', 'tremendo!', sep='-')
+
+print()
+
+print('{} es {}'.format('python','tremendo!'))
+
+print()
+
+numeros = [2, 3, 5, 7, 11]
+
+print(numeros)
+
+for n in numeros:
+    print(n)
+
+capitales = {'Colombia': 'Bogota', 'Peru': 'Lima'}
+
+print(capitales)
